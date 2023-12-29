1588f39ec84224d353cdaee3cc2f2ca667d33ae8
ehxa <2042918@student.uma.pt>
2023-12-29 13:02:24 +0000

    Resources definition

--- updateDeploy.sh
+++ updateDeploy.sh
@@ -3,22 +3,35 @@
 COMMIT_HASH=$(git rev-parse HEAD)
 DEPLOYMENT_SERVER_FILE="k8s-configuration/deployment-server.yml"
 DEPLOYMENT_CLIENT_FILE="k8s-configuration/deployment-client.yml"
+DEPLOYMENT_MYSQL_FILE="k8s-configuration/deployment-mysql.yml"
 GITHUB_ACTIONS_FILE=".github/workflows/main.yml" 
 
 OS="$(uname)"
-if [ "$OS" = "Darwin" ]; then
-    # macOS
-    sed -i '' "s|cesar02dd/server:.*|cesar02dd/server:$COMMIT_HASH|g" $DEPLOYMENT_SERVER_FILE
-    sed -i '' "s|cesar02dd/client:.*|cesar02dd/client:$COMMIT_HASH|g" $DEPLOYMENT_CLIENT_FILE
-    sed -i '' "s|\${{\ secrets.DOCKERHUB_USERNAME }}/server:.*|\${{\ secrets.DOCKERHUB_USERNAME }}/server:$COMMIT_HASH|g" $GITHUB_ACTIONS_FILE
-    sed -i '' "s|\${{\ secrets.DOCKERHUB_USERNAME }}/client:.*|\${{\ secrets.DOCKERHUB_USERNAME }}/client:$COMMIT_HASH|g" $GITHUB_ACTIONS_FILE
-else
-    # Linux 
-    sed -i "s|cesar02dd/server:.*|cesar02dd/server:$COMMIT_HASH|g" $DEPLOYMENT_SERVER_FILE
-    sed -i "s|cesar02dd/client:.*|cesar02dd/client:$COMMIT_HASH|g" $DEPLOYMENT_CLIENT_FILE
-    sed -i '' "s|\${{\ secrets.DOCKERHUB_USERNAME }}/server:.*|\${{\ secrets.DOCKERHUB_USERNAME }}/server:$COMMIT_HASH|g" $GITHUB_ACTIONS_FILE
-    sed -i '' "s|\${{\ secrets.DOCKERHUB_USERNAME }}/client:.*|\${{\ secrets.DOCKERHUB_USERNAME }}/client:$COMMIT_HASH|g" $GITHUB_ACTIONS_FILE
-fi
 
-echo "Deployment files and GitHub Actions workflow updated: $COMMIT_HASH"
+update_file() {
+    local file=$1
+    local pattern=$2
+    local replacement=$3
+    local os=$4
 
+    if [ "$os" = "Darwin" ]; then
+        sed -i '' "s|$pattern|$replacement|g" "$file"
+    else
+        sed -i "s|$pattern|$replacement|g" "$file"
+    fi
+
+    if [ $? -eq 0 ]; then
+        echo "File update successfully: $file"
+    else
+        echo "Error updating file: $file"
+    fi
+}
+
+update_file $DEPLOYMENT_SERVER_FILE "user/server:.*" "user/server:$COMMIT_HASH" $OS
+update_file $DEPLOYMENT_CLIENT_FILE "user/client:.*" "user/client:$COMMIT_HASH" $OS
+update_file $DEPLOYMENT_MYSQL_FILE "user/mysql:.*" "user/mysql:$COMMIT_HASH" $OS
+update_file $GITHUB_ACTIONS_FILE "\${{\ secrets.DOCKERHUB_USERNAME }}/server:.*" "\${{\ secrets.DOCKERHUB_USERNAME }}/server:$COMMIT_HASH" $OS
+update_file $GITHUB_ACTIONS_FILE "\${{\ secrets.DOCKERHUB_USERNAME }}/client:.*" "\${{\ secrets.DOCKERHUB_USERNAME }}/client:$COMMIT_HASH" $OS
+update_file $GITHUB_ACTIONS_FILE "\${{\ secrets.DOCKERHUB_USERNAME }}/mysql:.*" "\${{\ secrets.DOCKERHUB_USERNAME }}/mysql:$COMMIT_HASH" $OS
+
+echo "Files updated with hash: $COMMIT_HASH"
