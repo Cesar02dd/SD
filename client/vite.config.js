1cc860464fe1c50a8fe2974064fa766e7350f942
Cesar02dd <80769989+Cesar02dd@users.noreply.github.com>
2024-01-02 16:55:32 +0000

    Yesss, test with service name

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/register/, ''),
       },
       '/event': {
-        target: 'http://10.2.15.143:30150',
+        target: 'http://webapp-service:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/event/, ''),
       },
